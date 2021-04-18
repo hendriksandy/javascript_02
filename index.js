1c8f952c12241873911461a9e8152282eea5c81d
hendriksandy <79459594+hendriksandy@users.noreply.github.com>
2021-04-18 23:36:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hallo Winc Academy studenten")
+//dit is een grote som
+let multiply = (1230941 * 1823794)
+console.log(multiply)
+console.log(637263 / 54)
+//je kan de uitkomst van de comment niet zien , want die word overgeslagen
+/*let multiply = (1230941 * 1823794)
+console.log(multiply)
+console.log(637263 / 54)*/
